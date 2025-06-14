c71611da807d4bda08c8d805cde5295b5de7c6d0
abdo talat <Abdo.talaat740@gmail.com>
2025-06-14 20:57:28 +0300

    remove internal logic about the local date, the background modifier, and text color

--- app/src/main/java/com/example/tudee/presentation/components/DayCard.kt
+++ app/src/main/java/com/example/tudee/presentation/components/DayCard.kt
@@ -1,9 +1,11 @@
 package com.example.tudee.presentation.components
 
 import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
@@ -14,69 +16,39 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.platform.LocalConfiguration
 import com.example.tudee.designsystem.theme.TudeeTheme
-import java.time.LocalDate
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun DayCard(
     modifier: Modifier = Modifier,
-    isSelected: Boolean,
-    localDate: LocalDate
+    dayOfMonth: String,
+    dayOfWeek: String,
+    dayOfMonthTextColor: Color,
+    dayOfWeekTextColor: Color,
+    onClick: () -> Unit
 ) {
-    val configuration = LocalConfiguration.current
-    val screenWidth = configuration.screenWidthDp
-
-    val horizontalPadding = (screenWidth * 0.03f).dp
-    val verticalPadding = (screenWidth * 0.02f).dp
-
-    val gradientBackgroundColor = Brush.linearGradient(
-        colors = TudeeTheme.color.primaryGradient,
-        start = Offset(0f, 0f),
-        end = Offset(0f, Float.POSITIVE_INFINITY)
-    )
-
-    val appliedModifier = if (isSelected) {
-        modifier.background(
-            gradientBackgroundColor,
-            shape = RoundedCornerShape(16.dp)
-        )
-    } else {
-        modifier.background(
-            TudeeTheme.color.textColors.surface,
-            shape = RoundedCornerShape(16.dp)
-        )
-    }
-
-    val dayOfMonthTextColor =
-        if (isSelected)
-            TudeeTheme.color.textColors.onPrimary
-        else
-            TudeeTheme.color.textColors.body
-
-    val dayOfWeekTextColor =
-        if (isSelected)
-            TudeeTheme.color.textColors.onPrimaryCaption
-        else
-            TudeeTheme.color.textColors.hint
 
     Column(
-        modifier = appliedModifier
-            .padding(vertical = verticalPadding, horizontal = horizontalPadding),
+        modifier = modifier
+            .padding(vertical = 12.dp, horizontal = 14.dp)
+            .clickable {
+                onClick()
+            },
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
         Text(
-            text = localDate.dayOfMonth.toString(),
+            text = dayOfMonth,
             style = TudeeTheme.textStyle.title.medium,
             color = dayOfMonthTextColor
         )
 
         Text(
-            text = localDate.dayOfWeek.toString().take(3),
+            text = dayOfWeek,
             style = TudeeTheme.textStyle.body.small,
             modifier = Modifier.padding(top = 2.dp),
             color = dayOfWeekTextColor
@@ -86,7 +58,51 @@ fun DayCard(
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Preview(uiMode = UI_MODE_NIGHT_NO)
+@Preview(uiMode = UI_MODE_NIGHT_YES)
 @Composable
 fun DayCardPreview() {
-    DayCard(isSelected = true, localDate = LocalDate.of(2025, 6, 14))
+    TudeeTheme {
+        val gradientBackgroundColor = Brush.linearGradient(
+            colors = TudeeTheme.color.primaryGradient,
+            start = Offset(0f, 0f),
+            end = Offset(0f, Float.POSITIVE_INFINITY)
+        )
+
+        val isSelected = true
+
+        val dayOfMonthTextColor =
+            if (isSelected)
+                TudeeTheme.color.textColors.onPrimary
+            else
+                TudeeTheme.color.textColors.body
+
+        val dayOfWeekTextColor =
+            if (isSelected)
+                TudeeTheme.color.textColors.onPrimaryCaption
+            else
+                TudeeTheme.color.textColors.hint
+
+        val modifier = if (isSelected) {
+            Modifier.background(
+                gradientBackgroundColor,
+                shape = RoundedCornerShape(16.dp)
+            )
+        } else {
+            Modifier
+                .background(
+                    TudeeTheme.color.textColors.surface,
+                    shape = RoundedCornerShape(16.dp)
+                )
+        }
+
+        DayCard(
+            dayOfMonth = "14",
+            dayOfWeek = "SAT",
+            modifier = modifier,
+            dayOfMonthTextColor = dayOfMonthTextColor,
+            dayOfWeekTextColor = dayOfWeekTextColor,
+            onClick = {}
+        )
+    }
+
 }
