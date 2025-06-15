b3d3f5dc509727a4ba10b92178b6a30ec816e908
Rodina Mo'men <Rodinamobark3@gmail.com>
2025-06-15 07:52:06 +0300

    Add CategoryService for for ISP

--- app/src/main/java/com/example/tudee/domain/TaskService.kt
+++ app/src/main/java/com/example/tudee/domain/TaskService.kt
@@ -1,24 +1,19 @@
 package com.example.tudee.domain
 
 import com.example.tudee.domain.entity.Task
-import com.example.tudee.domain.entity.TaskCategory
 import com.example.tudee.domain.entity.TaskStatus
-import com.example.tudee.domain.request.TaskRequest
+import com.example.tudee.domain.request.TaskCreationRequest
 import kotlinx.datetime.LocalDate
 
 interface TaskService {
-    suspend fun createTask(taskRequest: TaskRequest)
-    suspend fun deleteTask(taskId: Int)
+    suspend fun createTask(taskCreationRequest: TaskCreationRequest)
+    suspend fun deleteTask(taskId: Long)
     suspend fun editTask(task: Task)
+    suspend fun editTaskStatus(taskId: Long, status: TaskStatus)
     suspend fun getTasks(): List<Task>
-    suspend fun getTaskById(taskId: Int): Task
-    suspend fun getTasksByCategoryId(categoryId: Int):List<Task>
+    suspend fun getTaskById(taskId: Long): Task
+    suspend fun getTasksByCategoryId(categoryId: Long): List<Task>
     suspend fun getTasksByStatus(status: TaskStatus): List<Task>
     suspend fun getTasksByAssignedDate(date: LocalDate): List<Task>
-    suspend fun getTasksCountByCategoryId(categoryId: Int): Int
-    suspend fun createCategory(category: TaskCategory)
-    suspend fun editCategory(category: TaskCategory)
-    suspend fun deleteCategory(categoryId: Int)
-    suspend fun getCategories(): List<TaskCategory>
-    suspend fun editTaskStatus(taskId: Int, status: TaskStatus)
+    suspend fun getTasksCountByCategoryId(categoryId: Long): Long
 }
--- app/src/main/java/com/example/tudee/domain/request/TaskRequest.kt
+++ app/src/main/java/com/example/tudee/domain/request/TaskRequest.kt
@@ -1,15 +1,14 @@
 package com.example.tudee.domain.request
 
-import com.example.tudee.domain.entity.TaskCategory
 import com.example.tudee.domain.entity.TaskPriority
 import com.example.tudee.domain.entity.TaskStatus
 import kotlinx.datetime.LocalDate
 
-data class TaskRequest(
+data class TaskCreationRequest(
     val title: String,
     val description: String,
     val priority: TaskPriority,
-    val category: TaskCategory?,
+    val categoryId: Long,
     val status: TaskStatus,
     val assignedDate: LocalDate
 )
