0c8e7c18bc439835d4c136364cbfd9dd3106ee5f
Amr Ashraf <AmrAshraf.info@gmail.com>
2025-06-15 23:13:35 +0300

    modify Tudee Chip to be more reusable

--- app/src/main/java/com/example/tudee/presentation/components/PriorityChip.kt
+++ app/src/main/java/com/example/tudee/presentation/components/PriorityChip.kt
@@ -2,6 +2,7 @@ package com.example.tudee.presentation.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -17,18 +18,25 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import com.example.tudee.designsystem.theme.TudeeTheme
 
 @Composable
 fun TudeeChip(
     label: String,
+    icon: Painter,
     modifier: Modifier = Modifier,
-    icon: ImageVector? = null,
     backgroundColor: Color = TudeeTheme.color.primary,
     textColor: Color = TudeeTheme.color.textColors.onPrimary,
+    textSize: TextUnit = TudeeTheme.textStyle.label.small.fontSize,
+    paddingValues: PaddingValues = PaddingValues(horizontal = 8.dp, vertical = 6.dp),
+    iconSize: Dp = 12.dp
 ) {
     Surface(
         modifier = modifier,
@@ -36,22 +44,20 @@ fun TudeeChip(
         shape = MaterialTheme.shapes.large
     ) {
         Row(
-            modifier = Modifier.padding(horizontal = 8.dp, vertical = 6.dp),
+            modifier = Modifier.padding(paddingValues),
             verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(2.dp)
+            horizontalArrangement = Arrangement.spacedBy(4.dp)
         ) {
-            if (icon != null) {
-                Icon(
-                    imageVector = icon,
-                    contentDescription = label,
-                    tint = textColor,
-                    modifier = Modifier.size(12.dp)
-                )
-            }
+            Icon(
+                painter = icon,
+                contentDescription = label,
+                tint = textColor,
+                modifier = Modifier.size(iconSize)
+            )
             Text(
                 text = label,
                 color = textColor,
-                style = TudeeTheme.textStyle.label.small
+                style = TextStyle(fontSize = textSize)
             )
         }
     }
@@ -67,19 +73,19 @@ fun PriorityChipPreview() {
         Column(verticalArrangement = Arrangement.spacedBy(8.dp), horizontalAlignment = Alignment.CenterHorizontally) {
             TudeeChip(
                 label = "High",
-                icon = Icons.Default.KeyboardArrowUp,
+                icon = rememberVectorPainter(Icons.Default.KeyboardArrowUp),
                 backgroundColor = colors.pinkAccent,
                 textColor = textColor
             )
             TudeeChip(
                 label = "Medium",
-                icon = Icons.Default.Warning,
+                icon = rememberVectorPainter(Icons.Default.Warning),
                 backgroundColor = colors.yellowAccent,
                 textColor = textColor
             )
             TudeeChip(
                 label = "Low",
-                icon = Icons.Default.KeyboardArrowDown,
+                icon = rememberVectorPainter(Icons.Default.KeyboardArrowDown),
                 backgroundColor = colors.greenAccent,
                 textColor = textColor
             )
