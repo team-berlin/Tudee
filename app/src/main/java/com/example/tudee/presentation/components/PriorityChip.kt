aadf7903faed13d888bfa3763d2f3bea9f6ff442
Amr Ashraf <AmrAshraf.info@gmail.com>
2025-06-16 21:13:10 +0300

    reduce Tudee Chip customization that affect the consistency.

--- app/src/main/java/com/example/tudee/presentation/components/PriorityChip.kt
+++ app/src/main/java/com/example/tudee/presentation/components/PriorityChip.kt
@@ -2,14 +2,9 @@ package com.example.tudee.presentation.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.KeyboardArrowUp
-import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -19,23 +14,22 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.graphics.vector.rememberVectorPainter
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import com.example.tudee.R
 import com.example.tudee.designsystem.theme.TudeeTheme
 
 @Composable
 fun TudeeChip(
     label: String,
-    icon: Painter,
     modifier: Modifier = Modifier,
+    labelColor: Color = TudeeTheme.color.textColors.onPrimary,
+    icon: Painter?,
     backgroundColor: Color = TudeeTheme.color.primary,
-    textColor: Color = TudeeTheme.color.textColors.onPrimary,
-    textSize: TextUnit = TudeeTheme.textStyle.label.small.fontSize,
-    paddingValues: PaddingValues = PaddingValues(horizontal = 8.dp, vertical = 6.dp),
+    labelSize: TextUnit = TudeeTheme.textStyle.label.small.fontSize,
     iconSize: Dp = 12.dp
 ) {
     Surface(
@@ -44,20 +38,23 @@ fun TudeeChip(
         shape = MaterialTheme.shapes.large
     ) {
         Row(
-            modifier = Modifier.padding(paddingValues),
+            modifier = Modifier.padding(horizontal = 8.dp, vertical = 6.dp),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.spacedBy(4.dp)
         ) {
-            Icon(
-                painter = icon,
-                contentDescription = label,
-                tint = textColor,
-                modifier = Modifier.size(iconSize)
-            )
+            if(icon != null){
+                Icon(
+                    painter = icon,
+                    contentDescription = label,
+                    tint = labelColor,
+                    modifier = Modifier.size(iconSize)
+                )
+            }
+
             Text(
                 text = label,
-                color = textColor,
-                style = TextStyle(fontSize = textSize)
+                color = labelColor,
+                style = TudeeTheme.textStyle.label.small.copy(fontSize = labelSize)
             )
         }
     }
@@ -73,21 +70,21 @@ fun PriorityChipPreview() {
         Column(verticalArrangement = Arrangement.spacedBy(8.dp), horizontalAlignment = Alignment.CenterHorizontally) {
             TudeeChip(
                 label = "High",
-                icon = rememberVectorPainter(Icons.Default.KeyboardArrowUp),
+                icon = painterResource(id = R.drawable.ic_priority_high),
                 backgroundColor = colors.pinkAccent,
-                textColor = textColor
+                labelColor = textColor
             )
             TudeeChip(
                 label = "Medium",
-                icon = rememberVectorPainter(Icons.Default.Warning),
+                icon = painterResource(id = R.drawable.ic_priority_medium),
                 backgroundColor = colors.yellowAccent,
-                textColor = textColor
+                labelColor = textColor
             )
             TudeeChip(
                 label = "Low",
-                icon = rememberVectorPainter(Icons.Default.KeyboardArrowDown),
+                icon = painterResource(id = R.drawable.ic_priority_low),
                 backgroundColor = colors.greenAccent,
-                textColor = textColor
+                labelColor = textColor
             )
         }
     }
