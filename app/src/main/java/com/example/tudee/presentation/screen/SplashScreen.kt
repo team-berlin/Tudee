4e6f278c994cbe952555f8fe5ebb675d9c4d1bf8
AbdoTalaat74 <abdo.talaat740@gmail.com>
2025-06-17 04:13:40 -0700

    splash screen implementation

---
+++ app/src/main/java/com/example/tudee/presentation/screen/SplashScreen.kt
@@ -0,0 +1,71 @@
+package com.example.tudee.presentation.screen
+
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.tudee.R
+import com.example.tudee.designsystem.theme.TudeeTheme
+import com.example.tudee.naviagtion.Destination
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(
+    modifier: Modifier = Modifier,
+    navController: NavController = rememberNavController(),
+    backgroundColor: Color = TudeeTheme.color.surface, // Use surface color as base
+    overlayColor: Color = TudeeTheme.color.statusColors.overlay,
+    backgroundPainter: Painter = painterResource(R.drawable.background_ellipse),
+    iconPainter: Painter = painterResource(R.drawable.tudee_logo)
+) {
+    LaunchedEffect(Unit) {
+        delay(3000)
+        navController.navigate(Destination.OnBoardingScreen.route) {
+            popUpTo(Destination.SplashScreen.route) { inclusive = true }
+        }
+    }
+
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(backgroundColor),
+        contentAlignment = Alignment.Center
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(overlayColor)
+        ) {
+            Image(
+                painter = backgroundPainter,
+                contentDescription = null,
+            )
+        }
+
+        Image(painter = iconPainter, contentDescription = null)
+    }
+
+
+}
+
+@Preview(showSystemUi = true, uiMode = UI_MODE_NIGHT_NO)
+@Preview(showSystemUi = true, uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun SplashScreenPreview() {
+    TudeeTheme {
+        SplashScreen()
+    }
+
+}
