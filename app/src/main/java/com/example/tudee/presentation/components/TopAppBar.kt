5957324c33041ae5523739432927b212693ef5ad
AbdoTalaat74 <abdo.talaat740@gmail.com>
2025-06-18 16:34:58 +0300

    - Implement custom `TudeeScaffold` composable - modify navBar and TopAppBar colors

--- app/src/main/java/com/example/tudee/presentation/components/TopAppBar.kt
+++ app/src/main/java/com/example/tudee/presentation/components/TopAppBar.kt
@@ -9,8 +9,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
