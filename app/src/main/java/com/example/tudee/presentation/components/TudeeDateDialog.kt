3b8206689b17803c17df82291923584ce3523e8b
Eman Abobakr <eman.abobakr@vodafone.com>
2025-06-20 00:45:57 +0300

    add TudeeDateDialogPreview

--- app/src/main/java/com/example/tudee/presentation/components/TudeeDateDialog.kt
+++ app/src/main/java/com/example/tudee/presentation/components/TudeeDateDialog.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tudee.R
 import com.example.tudee.presentation.composables.buttons.TextButton
@@ -68,4 +69,10 @@ fun TudeeDateDialog(
 
         }
     }
+}
+
+@Preview
+@Composable
+fun TudeeDateDialogPreview() {
+    TudeeDateDialog( { x -> }, {}, {})
 }
