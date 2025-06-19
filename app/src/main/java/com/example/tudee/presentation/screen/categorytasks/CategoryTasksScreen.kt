6fd79d27d0d4c5e8ccddc5c0b0ac86de0dc7f5eb
AbdoTalaat74 <abdo.talaat740@gmail.com>
2025-06-19 17:09:51 +0300

    remove unwanted method

--- app/src/main/java/com/example/tudee/presentation/screen/categorytasks/CategoryTasksScreen.kt
+++ app/src/main/java/com/example/tudee/presentation/screen/categorytasks/CategoryTasksScreen.kt
@@ -67,7 +67,7 @@ fun CategoryTasksScreen(
             modifier = modifier,
             categoryTaskUIState = uiState,
             onBackClick = { navController.navigateUp() },
-            onEditIconClick = { viewModel.showError() }
+            onEditIconClick = {  }
         )
 
         if (showSnackBar.value) {
