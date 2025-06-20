69130215e05a398748283b5f5311e38519f64e23
Eman Abobakr <eman.abobakr@vodafone.com>
2025-06-20 14:36:49 +0300

    add dark them in home

--- app/src/main/java/com/example/tudee/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/tudee/ui/home/screen/HomeScreen.kt
@@ -74,7 +74,7 @@ fun HomeScreen(
             viewModel.resetStatus()
         }
     }
-    TudeeTheme() {
+    TudeeTheme(isDarkTheme = state.isDarkMode) {
         HomeContent(
             navController = navController,
             modifier = modifier,
