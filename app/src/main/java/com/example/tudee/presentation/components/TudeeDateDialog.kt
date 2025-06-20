92db73517d1a3cc27e05d072545e5d5057de8d48
Eman Abobakr <eman.abobakr@vodafone.com>
2025-06-20 08:17:11 +0300

    update TudeeDateDialog ui

--- app/src/main/java/com/example/tudee/presentation/components/TudeeDateDialog.kt
+++ app/src/main/java/com/example/tudee/presentation/components/TudeeDateDialog.kt
@@ -1,7 +1,9 @@
 package com.example.tudee.presentation.components
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
@@ -10,12 +12,13 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tudee.R
-import com.example.tudee.presentation.composables.buttons.TextButton
+import com.example.tudee.designsystem.theme.TudeeTheme
 import com.example.tudee.utils.convertMillisToDate
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -44,29 +47,39 @@ fun TudeeDateDialog(
                 )
             },
         )
-        Row {
-            TextButton(onClick = {
-                onClear()
-            }) {
-                Text(stringResource(R.string.date_picker_clear_button_text))
-            }
-            Spacer(modifier = Modifier.weight(5f))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 12.dp, end = 40.dp, bottom = 20.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                modifier = Modifier.clickable { onClear() },
+                text = stringResource(R.string.date_picker_clear_button_text),
+                color = TudeeTheme.color.primary,
+                style = TudeeTheme.textStyle.label.large
+            )
 
-            TextButton(
-                onClick = {
-                    onDismiss()
-                }
-            ) {
-                Text(stringResource(R.string.date_picker_cancel_button_text))
-            }
-            Spacer(modifier = Modifier.weight(1f))
-            TextButton(onClick = {
-                onConfirm(dateState.selectedDateMillis)
-                onDismiss()
-            }) {
-                Text(stringResource(R.string.date_picker_ok_button_text))
-            }
+            Row(horizontalArrangement = Arrangement.spacedBy(35.dp)) {
 
+                Text(
+                    modifier = Modifier.clickable { onDismiss() },
+                    text = stringResource(R.string.date_picker_cancel_button_text),
+                    color = TudeeTheme.color.primary,
+                    style = TudeeTheme.textStyle.label.large
+                )
+                Text(
+                    modifier = Modifier.clickable {
+                        onConfirm(dateState.selectedDateMillis)
+                        onDismiss()
+                    },
+                    text = stringResource(R.string.date_picker_ok_button_text),
+                    color = TudeeTheme.color.primary,
+                    style = TudeeTheme.textStyle.label.large
+                )
+
+            }
         }
     }
 }
@@ -74,5 +87,5 @@ fun TudeeDateDialog(
 @Preview
 @Composable
 private fun TudeeDateDialogPreview() {
-    TudeeDateDialog( { _ -> }, {}, {})
+    TudeeDateDialog({ _ -> }, {}, {})
 }
