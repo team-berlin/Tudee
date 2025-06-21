1b386445ae52a104953486f07160a97dfe63964e
Eman Abobakr <eman.abobakr@vodafone.com>
2025-06-21 03:36:08 +0300

    add test coverage checker with kover

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,17 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
+    id("org.jetbrains.kotlinx.kover") version "0.9.0"
+}
 
+kover {
+    reports {
+        verify {
+            rule {
+                bound {
+                    minValue = 80
+                }
+            }
+        }
+    }
 }
