472f7d1da47d9161e4aca37fcb8de3eda16b41e0
Mostafa Ibrahim <mostafa1996998@gmail.com>
2025-06-25 12:05:51 +0300

    refactor CategoryUiState and CategoryMapper to use Long for id and String for image

--- app/src/main/java/com/example/tudee/presentation/screen/home/viewmodel/CategoryUiState.kt
+++ app/src/main/java/com/example/tudee/presentation/screen/home/viewmodel/CategoryUiState.kt
@@ -1,11 +1,8 @@
 package com.example.tudee.presentation.screen.home.viewmodel
 
-import androidx.annotation.DrawableRes
-
 data class CategoryUiState(
-    val id: String="0",
-    val title:String="",
-    @DrawableRes
-    val image: Int? = null,
-    val isPredefined: Boolean= false
+    val id: Long = 0L,
+    val title: String = "",
+    val image: String = "",
+    val isPredefined: Boolean = false
 )
