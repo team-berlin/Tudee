782962b17eb39e0595610cdd22ca6d03d1b15be6
AbdoTalaat74 <abdo.talaat740@gmail.com>
2025-06-27 04:38:07 +0300

    - add strings for category sheet

--- app/src/main/java/com/example/tudee/presentation/screen/splash/SplashScreen.kt
+++ app/src/main/java/com/example/tudee/presentation/screen/splash/SplashScreen.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -21,12 +23,15 @@ import com.example.tudee.R
 import com.example.tudee.designsystem.theme.TudeeTheme
 import com.example.tudee.domain.AppEntry
 import com.example.tudee.naviagtion.Destination
+import com.example.tudee.presentation.themeViewModel.ThemeViewModel
 import kotlinx.coroutines.delay
+import org.koin.androidx.compose.koinViewModel
 import org.koin.compose.getKoin
 
 @Composable
 fun SplashScreen(
     modifier: Modifier = Modifier,
+    themeViewModel: ThemeViewModel = koinViewModel(),
     navController: NavController = rememberNavController(),
     backgroundColor: Color = TudeeTheme.color.surface,
     overlayColor: Color = TudeeTheme.color.statusColors.overlay,
@@ -34,6 +39,7 @@ fun SplashScreen(
     iconPainter: Painter = painterResource(R.drawable.tudee_logo),
     appEntry: AppEntry = getKoin().get()
 ) {
+    val isDark by themeViewModel.isDarkMode.collectAsState()
     LaunchedEffect(Unit) {
         delay(3000)
         if (appEntry.isFirstEntry()) {
@@ -60,7 +66,7 @@ fun SplashScreen(
         ) {
             Image(
                 modifier = Modifier.fillMaxSize(),
-                painter = backgroundPainter,
+                painter = if (isDark) painterResource(R.drawable.background_ellipse_dark) else backgroundPainter,
                 contentDescription = null,
                 contentScale = ContentScale.Crop
             )
