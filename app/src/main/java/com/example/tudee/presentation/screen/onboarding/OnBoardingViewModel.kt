5df849527b3f277848d1d0ea6cd016e7c7f03720
Yusuf Nasser <yusufnassereng@gmail.com>
2025-06-27 17:39:51 +0300

    remove redundant code checking first entry in OnBoardingScreen
    Splash screen already does this step.

--- app/src/main/java/com/example/tudee/presentation/screen/onboarding/OnBoardingViewModel.kt
+++ app/src/main/java/com/example/tudee/presentation/screen/onboarding/OnBoardingViewModel.kt
@@ -5,20 +5,13 @@ import androidx.lifecycle.viewModelScope
 import com.example.tudee.data.preferences.PreferencesManager
 import com.example.tudee.domain.TaskCategoryService
 import com.example.tudee.presentation.utils.predefinedCategories
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class OnBoardingViewModel(
     private val taskCategoryService: TaskCategoryService,
     private val prefs: PreferencesManager
 ) : ViewModel() {
-
-    private val _isFirstEntry = MutableStateFlow(prefs.isOnboardingCompleted())
-    val isFirstEntry = _isFirstEntry.asStateFlow()
-
     fun saveFirstEntry() = prefs.setOnboardingCompleted()
-
     fun loadInitialData() {
         viewModelScope.launch {
             predefinedCategories.forEach { taskCategoryService.createCategory(it) }
